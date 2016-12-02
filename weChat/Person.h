c7c0a69b3ad2802ba9188fd39d173f26c1a4da7d
zph <zph@163.com>
2016-12-02 14:17:21 +0800

    创建了Person类

---
+++ weChat/Person.h
@@ -0,0 +1,13 @@
+//
+//  Person.h
+//  weChat
+//
+//  Created by admin on 16/12/2.
+//  Copyright © 2016年 FMB. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Person : NSObject
+
+@end
---
+++ weChat/Person.m
@@ -0,0 +1,13 @@
+//
+//  Person.m
+//  weChat
+//
+//  Created by admin on 16/12/2.
+//  Copyright © 2016年 FMB. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+@end
